99b82fdd7951791700fe9b86215d242b2cfaa75b
southwolf <wp.southwolf@gmail.com>
2017-09-26 18:02:20 +0800

    heroku node build

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env sh
 
+cd ./frontend; yarn
+cd ./frontend; npm run build
+
 bundle exec rake db:migrate
-bundle exec rake vue:install
-bundle exec rake vue:build
 bundle exec rake run
